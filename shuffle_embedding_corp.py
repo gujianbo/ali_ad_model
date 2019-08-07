8a4d3006d49cb1ec2b49441161bf370cb654befd
jianbogu <gujianbobo@gmail.com>
2019-08-07 23:49:56 +0800

    add shuffle

---
+++ shuffle_embedding_corp.py
@@ -0,0 +1,42 @@
+import sys,time,os
+import hashlib
+
+def readfile(fold,outfile):
+	fdout={}
+	filelist=[]
+	for filename in os.listdir(fold):
+		print(filename)
+		if filename.endswith("_corp"):
+			filelist.append(filename)
+	filelist.sort()
+	print(filelist)
+	for filename in filelist:
+		print("process "+filename)
+		with open(fold+filename,"r") as fd:
+			cnt=0
+			for line in fd:
+				cnt+=1
+				if cnt%1000000==0:
+					print(str(cnt)+" lines")
+				try:
+					line=line.strip()
+					(user,words)=line.split("\t")
+					bk = bucket(user)
+					if bk not in fdout:
+						fdout[bk] =open(outfile+"/"+str(bk),"w")
+					fdout[bk].write(line+"\n")
+				except:
+					print("Error line:",line)
+					continue
+	
+	for k in fdout.keys():
+		fdout[k].close()
+
+def bucket(user):
+	hash_md5 = hashlib.md5(user)
+	hash_md5_hex = hash_md5.subString(15,20)
+	return int(hash_md5_hex, 16)%16
+
+if __name__=="__main__":
+	readfile(sys.argv[1],sys.argv[2])
+	
